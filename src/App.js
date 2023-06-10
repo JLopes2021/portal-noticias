4af9537d7ab6dd9fd962421079dda5441318fc5d
JLopes2021 <jeffinho.je8@gmail.com>
2023-06-10 04:05:02 -0300

    landing-page-ok

--- src/App.js
+++ src/App.js
@@ -3,27 +3,31 @@ import axios from 'axios';
 import './App.css';
 
 function App() {
-const [posts,setPosts] = useState([]);
+  const [posts, setPosts] = useState([]);
 
-useEffect(()=>{
-  axios.get('http://localhost/noticiasweb/')
-  .then(function(res){
-    setPosts(res.data);
-  })
-},[])
+  useEffect(() => {
+    axios.get('http://localhost/noticiasweb/')
+      .then(function (res) {
+        setPosts(res.data);
+      })
+  }, [])
 
   return (
     <div className="App">
-     {
-      posts.map(function(val){
-        return(
-         <div className='container-noticia'>
-            <img src={val.imagem}/>
-            <p>{val.noticia}</p>
-         </div>
-        )
-      })
-     }
+      {
+        posts.map(function (val) {
+          return (
+            <div className='container-noticia'>
+              <figure>
+                <img src={val.imagem} />
+                <figcaption>{val.rodape}</figcaption>
+              </figure>
+              <p>{val.noticia}</p>
+
+            </div>
+          )
+        })
+      }
     </div>
   );
 }
